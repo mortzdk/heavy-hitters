8ed482aa6025ba2849a0d9746c32c68939a8facc
hovmand <jonas@hovmand.eu>
2016-03-14 14:18:32 +0100

    Moves median functions to an utility package

--- src/sketch/count_median.c
+++ src/sketch/count_median.c
@@ -11,142 +11,7 @@
 #include "xutil.h"
 #include "sketch/count_median.h"
 #include "hash.h"
-
-/* http://ndevilla.free.fr/median/median/index.html */
-
-/*
- *  This Quickselect routine is based on the algorithm described in
- *  "Numerical recipes in C", Second Edition,
- *  Cambridge University Press, 1992, Section 8.5, ISBN 0-521-43108-5
- *  This code by Nicolas Devillard - 1998. Public domain.
- */
-
-#define SWAP(a, b) { register int64_t t=(a);(a)=(b);(b)=t; }
-
-static int64_t quick_select(int64_t *restrict v, const uint32_t n) {
-    register uint32_t middle, ll, hh;
-    register uint32_t low          = 0;
-	register uint32_t high         = n-1;
-    register const uint32_t median = (low + high) / 2;
-
-    for (;;) {
-        if ( unlikely(high <= low) ) {
-			/* One element only */
-            return v[median];
-		}
-
-        if ( unlikely(high == low + 1) ) {
-			/* Two elements only */
-            if ( v[low] > v[high] ) {
-                SWAP(v[low], v[high]);
-			}
-            return v[median] ;
-        }
-
-		/* Find median of low, middle and high items; swap into position low */
-		middle = (low + high) / 2;
-
-		if ( v[middle] > v[high] ) {
-			SWAP(v[middle], v[high]);
-		}
-
-		if ( v[low] > v[high] ) {
-			SWAP(v[low], v[high]);
-		}
-
-		if ( v[middle] > v[low] ) {
-			SWAP(v[middle], v[low]);
-		}
-
-		/* Swap low item (now in position middle) into position (low+1) */
-		SWAP(v[middle], v[low+1]);
-
-		/* Nibble from each end towards middle, swapping items when stuck */
-		ll = low + 1;
-		hh = high;
-
-		for (;;) {
-			do {
-				ll++;
-			} while (v[low] > v[ll]);
-			do {
-				hh--;
-			} while (v[hh]  > v[low]);
-
-			if (hh < ll) {
-				break;
-			}
-
-			SWAP(v[ll], v[hh]) ;
-		}
-
-		/* Swap middle item (in position low) back into correct position */
-		SWAP(v[low], v[hh]) ;
-
-		/* Re-set active partition */
-		if ( hh <= median ) {
-			low  = ll;
-		}
-		if ( hh >= median ) {
-			high = hh - 1;
-		}
-    }
-}
-
-/*
- * Algorithm from N. Wirth's book, implementation by N. Devillard.
- * This code is public domain.
- */
-
-/*---------------------------------------------------------------------------
-   Function :   wirth()
-   In       :   array of elements, # of elements in the array
-   Out      :   one element
-   Job      :   find the median element in the array
-
-                Reference:
-
-                  Author: Wirth, Niklaus 
-                   Title: Algorithms + data structures = programs 
-               Publisher: Englewood Cliffs: Prentice-Hall, 1976 
-    Physical description: 366 p. 
-                  Series: Prentice-Hall Series in Automatic Computation 
-
- ---------------------------------------------------------------------------*/
-
-static int64_t wirth(int64_t *restrict v, const uint32_t n) {
-    register uint32_t i, j, l, m;
-    register int64_t x;
-	register const uint32_t k = (n-1)/2;
-
-    l = 0;
-	m = n-1;
-    while (l < m) {
-        x = v[k];
-        i = l;
-        j = m;
-        do {
-            while ( v[i] < x ) {
-				i++;
-			}
-            while ( x < v[j] ) {
-				j--;
-			}
-            if ( i <= j ) {
-                SWAP(v[i], v[j]);
-                i++;
-				j--;
-            }
-        } while ( i <= j );
-        if ( j < k ) {
-			l = i;
-		}
-        if ( k < i ) {
-			m = j;
-		}
-    }
-    return v[k];
-}
+#include "util/median.h"
 
 /*****************************************************************************
  *                     Count-Median Sketch Data Structure                    *
@@ -248,8 +113,8 @@ int64_t count_median_point(count_median_t *restrict s, const uint32_t i) {
 				(uint8_t)(table[di*(w+2)+1]));
 	}
 
-//	return quick_select(median, d);
-	return wirth(median, d);
+//	return median_quick_select(median, d);
+	return median_wirth(median, d);
 }
 
 int64_t count_median_range_sum(count_median_t *restrict s, const uint32_t l, 
